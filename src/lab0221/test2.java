0e6941d08910dcde9f23199f016d8dca6a801a6c
user <user@MH03>
2019-02-21 14:35:55 +0800

    test

---
+++ src/lab0221/test2.java
@@ -0,0 +1,10 @@
+package lab0221;
+import java.util.*;
+public class test2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
+	}
+}
